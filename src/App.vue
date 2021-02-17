e17f131b7f81814e248c71827a9f5b9f0f90adfa
2thousandblue <1244503766@qq.com>
2021-02-17 16:36:02 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view/>
+    <!--路由视图，当前路由指向的内容将显示在这个容器中。
+    其它的组件即使拥有自己的路由(URL，需要在 router 文件夹的 index.js 文件里定义)
+    也只不过表面上是一个单独的页面，实际上只是在根组件 App.vue 中。-->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+//ES6 的语法，将这个组件整体导出
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
+
 }
 </script>
 
@@ -23,6 +24,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
